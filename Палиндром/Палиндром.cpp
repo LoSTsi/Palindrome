e7fd3b38cb512a6899f81e9392fdf72a11ca8f1c
LoSTsi <167238154+LoSTsi@users.noreply.github.com>
2024-06-02 11:19:54 +0300

    Добавьте файлы проекта.

---
+++ Палиндром/Палиндром.cpp
@@ -0,0 +1,55 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	int longstr = 0, l = 0;
+	int i = 0;
+	bool palindrome = true;
+	string str, longstart, longend;
+	setlocale(LC_ALL, "rus");
+	cout << "Введите слово на проверку палиндрома" << endl;
+	cin >> str;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 1)
+	{
+		longstr = i / 2;
+		l = i / 2;
+		l++;
+		for (int j = 0; j < longstr; j++)
+		{
+			longstart = str[j];
+			l++;
+			for (;l < i; )
+			{
+				longend = str[l];
+				if (longstart != longend)
+				{
+					palindrome = false;
+					break;
+				}
+				if (longstart == longend)
+				{
+					break;
+				}
+			}
+		}
+	}
+	else
+	{
+		palindrome = false;
+	}
+
+	if (palindrome == true)
+	{
+		cout << "Ваше слово палиндром" << endl;
+	}
+	else
+	{
+		cout << "Ваше слово не палидром" << endl;
+	}
+}
